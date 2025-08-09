059dd87b39605f441e801d06894ec035400ce927
Aryan Singh chouhan <aryanchouhan691@gmail.com>
2025-08-09 13:44:55 +0530

    error changes

--- BackEnd/Index.js
+++ BackEnd/Index.js
@@ -11,25 +11,27 @@ import commentRooute from './Routes/CommentRoute.js';
 import publicRoute from './Routes/PublicRoute.js';
 
 DataBaseConnect();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static('public'));
+
 app.use(cors({
-  origin:true,
+  origin: ["http://localhost:5173", "https://your-frontend-domain.com"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
+
 app.use(cookieParser());
 
+app.use("/auth", AuthUserRoute);
+app.use("/blog", blogsRoute);
+app.use("/dashboard", dashBoardRoute);
+app.use("/comment", commentRooute);
+app.use("/public", publicRoute);
 
-app.use("/auth",AuthUserRoute);
-app.use('/blog',blogsRoute);
-app.use('/dashboard',dashBoardRoute);
-app.use('/comment',commentRooute);
-app.use('/public',publicRoute);
-
-app.listen(PORT, ()=>{
-  console.log(`server run on http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`server run on http://localhost:${PORT}`);
+});
